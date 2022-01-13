36e601fc93dd794efc234fa7b53b9aaf9b55ddeb
Thiago Pinheiro <pinheiro.thiago7@gmail.com>
2022-01-13 05:47:32 -0300

    primeiro commit deste projeto, porem com erro ao logar

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'config.php';
+require 'models/Auth.php';
+
+$auth = new Auth( $pdo, $base );
+$userInfo = $auth->checkToken();
+
+
+echo 'index';
+echo '  -  '.$_SESSION['token'];
